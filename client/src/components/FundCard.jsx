707772d8121c1fcbb9388388a5266d22c0c13f8c
Abb Muhaimeen <bsse1423@iit.du.ac.bd>
2025-01-26 00:49:30 +0600

    j

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -14,7 +14,7 @@ const FundCard = ({
   handleClick,
 }) => {
   const remainingDays = daysLeft(deadline);
-
+ 
   return (
     <div
       className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer"
